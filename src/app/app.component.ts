c91f5935d20d04fbe3a98fed22003578a7916fc0
Bhargesh Patel <pbhargesh82@aol.com>
2024-03-09 02:16:48 +0530

    update_added_first_two_pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mahi-angular';
+
+  isPlay: boolean = false;
+  youAreMy: string = '';
+  showSunshineForm: boolean = false;
+
+  toogleIsPlay(): void {
+    this.isPlay = true;
+    setTimeout(() => {
+      this.showSunshineForm = true;
+    }, 2000);
+  }
+
+  onGoAhead():void {
+    if (this.youAreMy.trim().toLowerCase() === 'sunshine') {
+      console.log('Yes, right answer!')
+      //! TODO: Show a progress bar on right answer and plan pages ahead! Start a music if you can after these.
+    } else {
+      console.log('No, wrong answer!')
+      //! TODO: Show something like a message may be a hint.
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'first', component: FirstComponent},
+  {path: 'second', component: SecondComponent}
+];
